c10423fef0925e15dceb03b0c73263973b23d821
Mykhailo Panas <lv211184pmi@gmail.com>
2017-07-06 22:34:24 +0300

    homeworks 9 and 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  showImage: Array<boolean> = [true, true];
+
+  displayImage(event) {
+    let btnName: string = event.target.id;
+    if (btnName == 'btn1') {
+      return (this.showImage[0] = false) && (this.showImage[1] = true);
+    } else if (btnName == 'btn2') {
+      return (this.showImage[0] = true) && (this.showImage[1] = false);
+    } else {
+      return (this.showImage[0] = false) && (this.showImage[1] = false);
+    }  
+  }
 }
+
+
+
+
+
